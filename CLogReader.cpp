4dc3ed81d1385daddc12d986f1977dd420fcc7a2
vlad2010 <vlad2010@outlook.com>
2018-06-03 18:14:20 +0100

    Intial commit for Log Reader

---
+++ CLogReader.cpp
@@ -0,0 +1,256 @@
+﻿#include "stdafx.h"
+#include "CLogReader.h"
+#include <Windows.h>
+#include <string.h>
+
+CLogReader::CLogReader() {
+	m_memoryGranularity = GetGranularity();
+};
+
+CLogReader::~CLogReader() {
+	Close();
+}
+
+// открытие файла, false - ошибка
+bool CLogReader::Open(const char* filename) {
+	if (m_isFileOpened) {
+		Close();
+	}
+
+	m_fileHandle = CreateFileA(filename, GENERIC_READ, FILE_SHARE_READ, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+	if (m_fileHandle == INVALID_HANDLE_VALUE) {
+		return false;
+	};
+
+	bool res = GetFileSizeEx(m_fileHandle, &m_fileSize);
+	if (!res || !m_fileSize.QuadPart) {
+		CloseHandle(m_fileHandle);
+		return false;
+	}
+
+	m_mappingHandle = CreateFileMapping(m_fileHandle, 0, PAGE_READONLY, 0, 0, 0);
+	if (m_mappingHandle == 0) {
+		CloseHandle(m_fileHandle);
+		return false;
+	}
+
+	m_isFileOpened = true;
+	return true;
+}
+
+// закрытие файла
+void CLogReader::Close() {
+	if (m_isFileOpened) {
+
+		if (m_baseAddress)
+			UnmapFragment();
+
+		m_currentFilePos.QuadPart = 0;
+		CloseHandle(m_mappingHandle);
+		CloseHandle(m_fileHandle);
+		m_isFileOpened = false;
+	}
+}
+
+// установка фильтра строк, false - ошибка
+bool CLogReader::SetFilter(const char *filter) {
+
+	char prev = 0;
+	size_t mask_pos = 0;
+	if (m_isMaskSet)
+		memcpy(m_maskCopy, m_mask, m_maskSize);
+
+	while (*filter != 0) {
+
+		if (mask_pos >= m_maskSize) {
+			//copy saved mask back 
+			if (m_isMaskSet)
+				memcpy(m_mask, m_maskCopy, m_maskSize);
+			return false;
+		}
+
+		if (prev != '*' || *filter != '*') {
+			m_mask[mask_pos] = *filter;
+			mask_pos++;
+		}
+
+		prev = *filter;
+		filter++;
+	}
+
+	if (mask_pos == 0) {
+		//copy saved mask back 
+		if (m_isMaskSet)
+			memcpy(m_mask, m_maskCopy, m_maskSize);
+		return false;
+	}
+
+	//end of line for our mask 
+	m_mask[mask_pos] = 0;
+	m_isMaskSet = true;
+	return true;
+}
+
+bool CLogReader::GetNextLine(char *buf, const int bufsize) {
+
+	bool matchResult = false;
+	if (m_isFileOpened && buf != nullptr && bufsize > 0) {
+
+		if (!m_data) {
+			if (!MapNextFragmentWithOffset(0))
+				return false;
+		}
+
+		while ((matchResult == false) && (m_currentFilePos.QuadPart < m_fileSize.QuadPart)) {
+			int bufPos = 0;
+			char symbol = 0;
+			const char *stringStart = nullptr;
+			while (1) {
+
+				if (m_currentMappingPos.QuadPart == m_mappedSize) {
+					if (!UnmapFragment())
+						return false;
+
+					if (m_currentFilePos.QuadPart < m_fileSize.QuadPart) {
+
+						if (!MapNextFragmentWithOffset(bufPos)) {
+							return false;
+						}
+						bufPos = 0;
+						symbol = 0;
+						stringStart = nullptr;
+						continue;
+					}
+					else {
+						return false;  //no more lines
+					}
+				}
+				if (symbol == 0) {
+					stringStart = &m_data[m_currentMappingPos.QuadPart];
+				}
+
+				symbol = m_data[m_currentMappingPos.QuadPart];
+				m_currentMappingPos.QuadPart++;
+				m_currentFilePos.QuadPart++;
+
+				if (symbol == '\r' || symbol == '\n' || symbol == 0) {
+					break;
+				}
+
+				if (bufPos < (int)(m_stringBufSize - 1)) {
+					bufPos++;
+				}
+			}
+
+			if (bufPos > 0) {
+				matchResult = Match(m_mask, stringStart);
+				if (matchResult) {
+					unsigned int sizeToCopy = (bufPos<bufsize-1)? bufPos : bufsize;
+					memcpy(buf, stringStart, sizeToCopy);
+					buf[sizeToCopy] = 0;
+				}
+			}
+			else {
+				matchResult = false;
+			}
+		}
+
+		if (m_currentFilePos.QuadPart == m_fileSize.QuadPart) {
+			UnmapFragment();
+		}
+	}
+	return matchResult;
+}
+
+bool CLogReader::Match(const char* mask, const char* str)
+{
+	const char *strP, *maskP;
+	bool star = false;
+loopStart:
+	for (strP = str, maskP = mask; *strP != '\r' && *strP != '\n'; ++strP, ++maskP) {
+		switch (*maskP) {
+		case '?':
+			if (*strP == '.')
+				goto starCheck;
+			break;
+		case '*':
+			star = true;
+			str = strP, mask = maskP;
+			if (!*++mask)
+				return true;
+			goto loopStart;
+		default:
+			if (*strP != *maskP)
+				goto starCheck;
+			break;
+		}
+	}
+	if (*maskP == '*')
+		++maskP;
+	return (!*maskP);
+
+starCheck:
+	if (!star)
+		return false;
+	str++;
+	goto loopStart;
+}
+
+bool CLogReader::UnmapFragment() {
+	bool result = true;
+
+	if (m_baseAddress) {
+		result = UnmapViewOfFile(m_baseAddress);
+
+		m_baseAddress = nullptr;
+		m_data = nullptr;
+		m_currentMappingPos.QuadPart = 0;
+		m_mappedSize = 0;
+
+		if (!result)
+			return false;
+	}
+	return result;
+};
+
+bool CLogReader::MapNextFragmentWithOffset(unsigned int offset) {
+
+	LARGE_INTEGER nextMapAddress = getProperAddressForReMap(m_currentFilePos.QuadPart, offset);
+	m_currentFilePos.QuadPart -= offset; //we go back to the beginning of the string
+
+	long long howMuchLeft = m_fileSize.QuadPart - nextMapAddress.QuadPart;
+	m_mappedSize = (unsigned int)(howMuchLeft > m_pageSize ? m_pageSize : m_fileSize.QuadPart - nextMapAddress.QuadPart);
+
+	m_baseAddress = MapViewOfFile(m_mappingHandle, FILE_MAP_READ, nextMapAddress.HighPart, nextMapAddress.LowPart, m_mappedSize);
+	if (m_baseAddress == 0) {
+		return false;
+	}
+
+	if (offset>0) {
+		m_currentMappingPos.QuadPart = m_memoryGranularity - offset;
+	}
+	else {
+		m_currentMappingPos.QuadPart = 0;
+	}
+	m_data = (const char*)m_baseAddress;
+	return true;
+};
+
+DWORD CLogReader::GetGranularity() {
+	SYSTEM_INFO si;
+	::GetNativeSystemInfo(&si);
+	return si.dwAllocationGranularity;
+}
+
+LARGE_INTEGER CLogReader::getProperAddressForReMap(long long currentFilePos, unsigned int offset) {
+
+	LARGE_INTEGER newMappingAddress{ 0 };
+	if (((currentFilePos-offset) % m_memoryGranularity) == 0) {
+		newMappingAddress.QuadPart = currentFilePos - offset;
+	}
+	else {
+		newMappingAddress.QuadPart = (((currentFilePos - offset) / m_memoryGranularity) * m_memoryGranularity);
+	}
+
+	return newMappingAddress;
+}
---
+++ CLogReader.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+#include <Windows.h>
+#include <memory>
+
+#define DEFAULT_MASK_SIZE 1024
+#define DEFAULT_STRBUF_SIZE 4096
+#define DEFAULT_PAGE_SIZE 134217728   //128M
+
+class CLogReader {
+public:
+	CLogReader();
+	~CLogReader();
+
+	bool    Open(const char *filename);                       // открытие файла, false - ошибка
+	void    Close();									      // закрытие файла
+
+	bool    SetFilter(const char *filter);                    // установка фильтра строк, false - ошибка
+	bool    GetNextLine(char *buf, const int bufsize);  // запрос очередной найденной строки,
+														// buf - буфер, bufsize - максимальная длина
+														// false - конец файла или ошибка
+private:
+	bool m_isFileOpened = false;
+	bool m_isMaskSet = false;
+
+	LPVOID m_baseAddress = nullptr;
+	const char* m_data = nullptr;
+	const char *m_filename = nullptr;
+
+	LARGE_INTEGER m_fileSize{ 0 };
+	LARGE_INTEGER m_currentFilePos{ 0 };
+	LARGE_INTEGER m_currentMappingPos{ 0 };
+
+	HANDLE m_mappingHandle = nullptr;
+	HANDLE m_fileHandle = nullptr;
+	const unsigned int m_pageSize = DEFAULT_PAGE_SIZE;
+
+	size_t  m_mappedSize = 0;
+	const unsigned int m_maskSize = DEFAULT_MASK_SIZE;
+	const unsigned int m_stringBufSize = DEFAULT_STRBUF_SIZE;
+
+	DWORD m_memoryGranularity = 0;
+
+	char m_mask[DEFAULT_MASK_SIZE];
+	char m_maskCopy[DEFAULT_MASK_SIZE];
+
+	bool Match(const char* wildcard, const char* string);
+	bool UnmapFragment();
+	bool MapNextFragmentWithOffset(unsigned int offset);
+
+	DWORD GetGranularity();
+	LARGE_INTEGER getProperAddressForReMap(long long currentFilePos, unsigned int offset);
+};
+
